69029b7f44ec38c18198b005410015bca2188b63
Peter-The-Great <PjotrW15@Gmail.com>
2023-10-06 15:59:08 +0200

    Add JWT

--- API-Demo/Controllers/AccountController.cs
+++ API-Demo/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
 ﻿namespace API_Demo.Controllers;
 using API_Demo.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 public class GebruikerMetWachtwoord : IdentityUser
 {
@@ -21,14 +27,39 @@ public class GebruikerLogin
 [ApiController]
 public class AccountController : ControllerBase
 {
+    //De variabelen die we nodig hebben voor de AccountController
+    //Dat zijn de UserManager en de SignInManager, UserManager is nodig om gebruikers te kunnen aanmaken en SignInManager is nodig om gebruikers te kunnen inloggen.
     private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
+
+    //Dit is de constructor van de AccountController
     public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
     {
         _userManager = userManager;
         _signInManager = signInManager;
     }
 
+    // Dit is de methode om alle gebruikers op te halen uit de database.
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        return Ok(users);
+    }
+    // Dit is de methode om een gebruiker op te halen uit de database.
+    [HttpGet("{id}")]
+    [Authorize]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+
+    // Hier vindt zich de registratie plaats waar we een gebruiker aanmaken en deze toevoegen aan de database.
     [HttpPost]
     [Route("registreer")]
     public async Task<ActionResult<IEnumerable<Vak>>> Registreer([FromBody] GebruikerMetWachtwoord gebruikerMetWachwoord)
@@ -36,17 +67,41 @@ public class AccountController : ControllerBase
         var resultaat = await _userManager.CreateAsync(gebruikerMetWachwoord, gebruikerMetWachwoord.Password);
         return !resultaat.Succeeded ? new BadRequestObjectResult(resultaat) : StatusCode(201);
     }
+
+    //Hier vindt zich de login plaats waar we een token aanmaken voor de gebruiker en deze terugsturen naar de gebruiker als deze correct is ingelogd.
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] GebruikerLogin gebruikerLogin)
     {
         var _user = await _userManager.FindByNameAsync(gebruikerLogin.UserName);
-
         if (_user != null)
-        {
-            await _signInManager.SignInAsync(_user, true);
-            return Ok();
-        }
+            if (await _userManager.CheckPasswordAsync(_user, gebruikerLogin.Password))
+            {
+                var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"));
+
+                var signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+                var claims = new List<Claim> { new Claim(ClaimTypes.Name, _user.UserName) };
+                var roles = await _userManager.GetRolesAsync(_user);
+                foreach (var role in roles)
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                var tokenOptions = new JwtSecurityToken
+                (
+                    issuer: "https://localhost:7047",
+                    audience: "https://localhost:7047",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(10),
+                    signingCredentials: signingCredentials
+                );
+                return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(tokenOptions) });
+            }
 
         return Unauthorized();
+    }
+
+    //Dit is de logout methode
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return Ok();
     }
 }
--- API-Demo/Program.cs
+++ API-Demo/Program.cs
@@ -1,7 +1,9 @@
 using API_Demo.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 //Here we create a builder for our application
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +17,39 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddDefaultTokenProviders();
 builder.Services.AddAuthentication();
 builder.Services.AddControllers();
+
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "https://localhost:7047",
+        ValidAudience = "https://localhost:7047",
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"))
+    };
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JWT Authorization header using the Bearer scheme."
+}));
 
 var app = builder.Build();
 Console.WriteLine(builder.Configuration.GetConnectionString("SchoolContext"));
